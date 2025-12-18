8f524917cf58502e8d549c9bf6fc6c5709179874
billavramidis <bavramidis04@gmail.com>
2025-12-18 18:57:15 +0200

    fix: jacobi using the original pixels

--- jacobi.py
+++ jacobi.py
@@ -21,28 +21,32 @@ def find_neighbours(i, j, channel, width, height):
 
     return left, right, up, bottom
 
-def jacobi_update(original_channel, i, j, width, height):
-    left, right, up, bottom = find_neighbours(i, j, original_channel, width, height)
+def jacobi_update(original_channel, current_channel, i, j, width, height):
+    left, right, up, bottom = find_neighbours(i, j, current_channel, width, height)
     return a * original_channel[i][j] + b * (left + right + up + bottom)
 
-def jacobi_method(current_pixels):
+def jacobi_method(original_pixels, current_pixels):
         height, width, _ = current_pixels.shape
 
-        current_red_channel = current_pixels[:,:,0]
-        current_green_channel = current_pixels[:,:,1]
-        current_blue_channel = current_pixels[:,:,2]
+        orig_r = original_pixels[:, :, 0]
+        orig_g = original_pixels[:, :, 1]
+        orig_b = original_pixels[:, :, 2]
 
-        new_red_channel = current_red_channel.copy()
-        new_green_channel = current_green_channel.copy()
-        new_blue_channel = current_blue_channel.copy()
+        curr_r = current_pixels[:,:,0]
+        curr_g = current_pixels[:,:,1]
+        curr_b = current_pixels[:,:,2]
+
+        new_r = curr_r.copy()
+        new_g = curr_g.copy()
+        new_b = curr_b.copy()
 
         for i in range(0, height):
             for j in range(0, width):
-                new_red_channel[i][j] = jacobi_update(current_red_channel, i, j, width, height)
-                new_green_channel[i][j] = jacobi_update(current_green_channel, i, j, width, height)
-                new_blue_channel[i][j] = jacobi_update(current_blue_channel, i, j, width, height)
+                new_r[i][j] = jacobi_update(orig_r, curr_r, i, j, width, height)
+                new_g[i][j] = jacobi_update(orig_g, curr_g, i, j, width, height)
+                new_b[i][j] = jacobi_update(orig_b, curr_b, i, j, width, height)
 
-        return np.dstack((new_red_channel, new_green_channel, new_blue_channel))
+        return np.dstack((new_r, new_g, new_b))
                 
 
 for noisy_image in image_locations:
@@ -52,10 +56,10 @@ for noisy_image in image_locations:
     with Image.open(noisy_image).convert("RGB") as img:
         original_pixels = np.array(img).astype(np.float64)
         
-        current_pixels = original_pixels
+        current_pixels = original_pixels.copy()
 
         for k in range(0, iterations):
-            new_pixels = jacobi_method(current_pixels)
+            new_pixels = jacobi_method(original_pixels, current_pixels)
             current_pixels = new_pixels.copy()
         
         result = np.clip(current_pixels, 0, 255).astype(np.uint8)
